ea45a33c0237a15364a2e0341362333eb900c027
Lsw <2293347358@qq.com>
2017-09-05 23:17:20 +0800

    GIT

---
+++ gitTest/src/myGit/tgit.java
@@ -0,0 +1,13 @@
+package myGit;
+
+public class tgit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO GIT!");
+	}
+
+}
